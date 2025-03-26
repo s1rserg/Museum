eaff3494cfbcfa018148b6883c39f845df61c433
sirserg <sirserg05@gmail.com>
2025-03-26 18:13:00 +0200

    make build

---
+++ Museum.004ab8bb.js
@@ -0,0 +1,2 @@
+"use strict";let swiper=new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{0:{slidesPerView:1.06755,spaceBetween:16},768:{slidesPerView:2.0805,spaceBetween:20}}});
+//# sourceMappingURL=Museum.004ab8bb.js.map
